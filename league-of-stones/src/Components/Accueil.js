364307e12ec2db62f3af0e4d0f6768413da0d61f
derfoufhajar <hajar.derfouf@gmail.com>
2022-04-15 17:39:04 +0200

    logout + delete account btn

--- league-of-stones/src/Components/Accueil.js
+++ league-of-stones/src/Components/Accueil.js
@@ -6,8 +6,12 @@ import { useState } from 'react';
 
 function Accueil(){
     return (
-        <h1>Accueil</h1>
-      
+       
+        <div>
+            <button id="logout">Deconnection</button>
+            <button id="deleteAccount">Supprimer compte</button>
+        </div>
+        
     )
 }
 
