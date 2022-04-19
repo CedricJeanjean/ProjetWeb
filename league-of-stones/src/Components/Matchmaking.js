7297911703c3f5ce461467d2d5aa5e237bc737e5
cedricmithyx <57490437+cedricmithyx@users.noreply.github.com>
2022-04-19 11:51:50 +0200

    debut de matchmaking
    Debut du matchmaking avec les api, il manque l'interface

--- league-of-stones/src/Components/Matchmaking.js
+++ league-of-stones/src/Components/Matchmaking.js
@@ -5,8 +5,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Matchmaking(){
 
+    const nameid = new Map();
+
     console.log(sessionStorage.getItem('token'))
-    
+
+    fetch('http://localhost:3001/matchmaking/participate', {
+            method: 'GET',
+            headers: {
+              'www-authenticate' : sessionStorage.getItem('token'),
+              'Content-Type': 'application/json'
+            },
+          }).then(result => result.json())
+          .then(result => {
+              console.log(result);
+          }
+    );
+
+    function getPlayer() {
+        fetch('http://localhost:3001/matchmaking/getAll', {
+            method: 'GET',
+            headers: {
+              'www-authenticate' : sessionStorage.getItem('token'),
+              'Content-Type': 'application/json'
+            },
+          }).then(result => result.json())
+          .then(result => {
+              for(let i = 0; i<result.length; i++){
+                  console.log(result[i].name);
+                  nameid.set(result[i].name, result[i].matchmakingId);
+              }
+          }
+    );
+    }
+
+    function askForPlay(name){
+        fetch('http://localhost:3001/matchmaking/request?matchmakingId='+nameid.get(name), {
+            method: 'GET',
+            headers: {
+              'www-authenticate' : sessionStorage.getItem('token'),
+              'Content-Type': 'application/json'
+            },
+          }).then(result => result.json())
+          .then(result => {
+              console.log(result);
+          }
+    );
+    }
+
+    getPlayer();
+
+
     return (
        
         <div>
