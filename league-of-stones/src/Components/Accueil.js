72491fa51ab05946f4f5a15a1634bcc567e27977
derfoufhajar <hajar.derfouf@gmail.com>
2022-04-19 09:14:13 +0200

    commit test on casse rien

--- league-of-stones/src/Components/Accueil.js
+++ league-of-stones/src/Components/Accueil.js
@@ -1,15 +1,25 @@
 import '../App.css';
+import './Accueil.css'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
+import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js';
 
 
 function Accueil(){
     return (
        
-        <div>
-            <button id="logout">Deconnection</button>
-            <button id="deleteAccount">Supprimer compte</button>
+        <div className="body">
+            
+            <a className="btn-logout " id="logout" href="" onClick={deconnection}>
+                <i class="fa-solid fa-right-from-bracket"></i>
+                <div className="logout">Deconnexion</div>
+            </a>
+            <div className="center">
+             <button className="btn-match red">Match</button>
+            </div>
+            <button id="deleteAccount" onClick={deleteAccount}> Supprimer compte</button>
+      
         </div>
         
     )
