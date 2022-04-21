35d30e2b708cccdb0f2574fac38cceecf030b6ac
louannecostes <louanne.costes@gmail.com>
2022-04-21 14:42:24 +0100

    cartes

--- league-of-stones/src/Components/ListPlayers.js
+++ league-of-stones/src/Components/ListPlayers.js
@@ -8,7 +8,6 @@ class ListPlayers extends React.Component {
             const liste  = this.props.liste;
             return  Object.keys(liste).map((key, index) => { 
                 return <div className="col-3 d-flex justify-content-center align-items-center">
-                
                 <div class="carte" onClick={() => {this.props.setAdv(liste[key].name)}}>Play with  {liste[key].name}</div> </div>
                 
                 
--- league-of-stones/src/Components/ListRequest.js
+++ league-of-stones/src/Components/ListRequest.js
@@ -8,7 +8,9 @@ class ListRequest extends React.Component {
             const liste  = this.props.liste;
             return  Object.keys(liste).map((key, index) => { 
                 return  <div className="col-3 d-flex justify-content-center align-items-center">
-                    <div class="carte" onClick={() => {this.props.setAdv(liste[key].name)}}>Play with  {liste[key].name}</div> </div>
+                        
+                    <div class="carte" onClick={() => {this.props.setAdv(liste[key].name)}}>Play with  {liste[key].name}</div> 
+                    </div>
                 
                  
             })
